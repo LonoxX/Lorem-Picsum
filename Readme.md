18d8d0822bc70d53ef6e76f8f483e02c87f8c9c8
LonoxX <lonoxX96@gmail.com>
2023-02-01 17:16:07 +0100

    add: new feat
    Add Custom Text Support
    Add Custom Text Color Support
    Add Custom Background Color Support
    Add Custom Image Format Support

--- Readme.md
+++ Readme.md
@@ -8,12 +8,38 @@ It's free to use, and you can use it for any project - even commercial projects.
 
 Lorem Picsum is a free service that provides placeholder images for you to use in your designs and create a simple placeholder image that you can use in your layouts while you work on your content.
 
+## Examples
+
+|  |  | |  |
+| --- | --- | --- | --- |
+| ![Lorem Picsum](https://pic.pnnet.dev/256x256) | ![Lorem Picsum](https://pic.pnnet.dev/256x256?text=Lorem%20Picsum) |![Lorem Picsum](https://pic.pnnet.dev/256x256?tc=red)| ![Lorem Picsum](https://pic.pnnet.dev/256x256?bgc=blue)
+
+# Usage
+
+### Parameters
+| Parameter | Description |
+| --- | --- |
+| text | Custom Text |
+| tc | Text Color |
+| bgc | Background Color |
+
+| Text & Background Color | Image Format |
+| --- | --- |
+black   | png | 
+white   | jpg |
+red     | gif |
+green   | webp |
+blue    ||
+yellow  ||
+grey    ||
+
+
 ## Roadmap
 
-- [ ] - Add Custom Text Support (Lorem Ipsum, Hello World, etc.)  
-- [ ] - Add Custom Text Color Support  
-- [ ] - Add Custom Background Color Support  
-- [ ] - Add Custom Image Format Support (jpg, png, webp, gif)
+- [x] - Add Custom Text Support (Lorem Ipsum, Hello World, etc.)  
+- [x] - Add Custom Text Color Support  
+- [x] - Add Custom Background Color Support  
+- [x] - Add Custom Image Format Support (jpg, png, webp, gif)
 
 # Support
 ### © 2023 Panda-Network
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
-header("Content-Type: image/png");
-list($width, $height) = explode("x", $_GET["size"]);
-$image = imagecreatetruecolor($width, $height);
-$color = imagecolorallocate($image, 122, 122, 122);
-imagefill($image, 0, 0, $color);
+// 400x400?text=1337&tc=black&bgc=grey&format=gif
 
-$text_color = imagecolorallocate($image, 255, 255, 255);
-$text = $width . "x" . $height;
+$format = isset($_GET['format']) && in_array($_GET['format'], ['jpg', 'png', 'webp', 'gif']) ? $_GET['format'] : 'png';
+header("Content-Type: image/{$format}");
+list($width, $height) = explode('x', $_GET['size']);
+
+$text = isset($_GET['text']) ? $_GET['text'] : "{$width}x{$height}";
+$text_color = isset($_GET['tc']) ? getColor($_GET['tc']) : [255, 255, 255];
+
+$bg_color = isset($_GET['bgc']) ? getColor($_GET['bgc']) : [122, 122, 122];
+
+$image = imagecreatetruecolor($width, $height);
+imagefill($image, 0, 0, imagecolorallocate($image, ...$bg_color));
+
 $font = 'font/arial.ttf';
 $font_size = 32;
+
 do {
   $font_size--;
   $text_box = imagettfbbox($font_size, 0, $font, $text);
@@ -19,7 +26,38 @@ $text_height = $text_box[3] - $text_box[1];
 $x = ($width - $text_width) / 2;
 $y = ($height - $text_height) / 2 + $text_height;
 
-imagettftext($image, $font_size, 0, $x, $y, $text_color, $font, $text);
+imagettftext($image, $font_size, 0, $x, $y, imagecolorallocate($image, ...$text_color), $font, $text);
 
-imagepng($image);
+switch ($format) {
+  case 'jpg':
+    imagejpeg($image);
+    break;
+  case 'webp':
+    imagewebp($image);
+    break;
+  case 'gif':
+    imagegif($image);
+    break;
+  case 'png':
+  default:
+    imagepng($image);
+    break;
+}
+
 imagedestroy($image);
+
+function getColor($color)
+{
+  $colors = array(
+    "black" => array(0, 0, 0),
+    "white" => array(255, 255, 255),
+    "red" => array(255, 0, 0),
+    "green" => array(0, 255, 0),
+    "blue" => array(0, 0, 255),
+    "yellow" => array(255, 255, 0),
+    "grey" => array(128, 128, 128)
+  );
+  if (isset($colors[$color])) {
+    return $colors[$color];
+  }
+}
